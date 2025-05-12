f7f856c5cdd2590626f109eb56634ae0c379b0b1
Pedro Ceriotti <pedro@nekt.ai>
2025-05-12 09:40:51 -0300

    fix: Revert batch size and cursor changes

--- tap_mongodb/collection.py
+++ tap_mongodb/collection.py
@@ -124,35 +124,14 @@ class CollectionStream(Stream):
         return Timestamp(first_record.generation_time, first_record._inc)
 
     def get_records(self, context: dict | None) -> Iterable[dict]:
-        user_logger.info(f"Using batch size of {self.config.get('batch_size', 1000)} records.")
-        user_logger.info(f"Cursor timeout is set to {self.config.get('no_cursor_timeout', False)}")
-        user_logger.info(f"Starting data extraction..")
         bookmark = self.get_starting_replication_key_value(context)
-        if self.config.get("batch_size"):
-            cursor = self._collection.find(
-                {self.replication_key: {"$gt": bookmark}} if bookmark else {},
-                no_cursor_timeout=self.config.get("no_cursor_timeout", False),
-                batch_size=self.config.get("batch_size", 1000),
-            )
-        else:
-            cursor = self._collection.find(
-                {self.replication_key: {"$gt": bookmark}} if bookmark else {},
-                no_cursor_timeout=self.config.get("no_cursor_timeout", False),
-            )
-
-        try:
-            for record in cursor:
-                if self._strategy == "envelope":
-                    # Return the record wrapped in a document key
-                    yield {"_id": record["_id"], "document": json.dumps(record, default=self._handle_unusual_types)}
+        for record in self._collection.find({self.replication_key: {"$gt": bookmark}} if bookmark else {}):
+            if self._strategy == "envelope":
+                # Return the record wrapped in a document key
+                yield {"_id": record["_id"], "document": json.dumps(record, default=self._handle_unusual_types)}
             else:
                 # Return the record as is
                 yield record
-        except Exception as e:
-            self.logger.error(f"Error fetching records: {e}")
-            raise e
-        finally:
-            cursor.close()
 
     def _handle_unusual_types(self, obj):
         if isinstance(obj, datetime.datetime):
